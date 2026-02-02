1c71096b0b30094fe4b608c93b5321f2df1b6390
Siddhant Chaware <siddhantchaware999@gmail.com>
2026-02-02 19:05:25 +0530

    Added show/hide password feature to login page

--- index.php
+++ index.php
@@ -1,73 +1,99 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+    <title>Login Page</title>
+</head>
 
+<body>
 
-<?php 
+<?php
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-if(isset($_POST['login']))
-{
-	if($username == 'Robin' && $password == '1234')
-	{
-		session_start();
-		$_SESSION['activeUser'] = $username;
-		header('Location:dashboard.php');
-	}
-	else
-	{
-		echo "<script>alert('Wrong')</script>";
-		header('Location:index.php');
-	}
+if(isset($_POST['login'])) {
+    if($username == 'Robin' && $password == '1234') {
+        session_start();
+        $_SESSION['activeUser'] = $username;
+        header('Location:dashboard.php');
+        exit();
+    } else {
+        echo "<script>alert('Wrong')</script>";
+    }
 }
- 
 ?>
-    
-    <div class="container mt-4">
-    	<form method="POST">
-		  <div class="mb-3">
-		    <label class="form-label">Username:</label>
-		    <input type="text" name="username" id="username" class="form-control">
-		   
-		  </div>
-		  <div class="mb-3">
-		    <label for="exampleInputPassword1" class="form-label">Password:</label>
-		    <input type="password" name="password" id="password" class="form-control">
-		  </div>
 
-		  <div class="mb-4 float-end">Don't have an account?<a href="registration.php">Register Here</a></div>
-	
-		  <button type="submit" name="login" class="btn btn-primary mt-4">Submit</button>
-		</form>
-    </div>
+<div class="container mt-4">
+    <form method="POST">
 
-    <div class="container mt-4">
-    	<div class="card" style="width: 18rem;">
-  <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+        <div class="mb-3">
+            <label class="form-label">Username:</label>
+            <input type="text" name="username" id="username" class="form-control">
+        </div>
+
+        <!-- Updated password field -->
+        <div class="mb-3">
+            <label class="form-label">Password:</label>
+
+            <div class="input-group">
+                <input type="password" name="password" id="password" class="form-control">
+
+                <button type="button"
+                        class="btn btn-outline-secondary"
+                        onclick="togglePassword(this)">
+                        Show
+                </button>
+            </div>
+        </div>
+
+        <div class="mb-4 float-end">
+            Don't have an account?
+            <a href="registration.php">Register Here</a>
+        </div>
+
+        <button type="submit" name="login" class="btn btn-primary mt-4">
+            Submit
+        </button>
+
+    </form>
 </div>
+
+<div class="container mt-4">
+    <div class="card" style="width: 18rem;">
+        <img src="https://picsum.photos/200" class="card-img-top">
+
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+                Some quick example text to build on the card title.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
     </div>
+</div>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 
-  
-  </body>
+<!-- Password toggle script -->
+<script>
+function togglePassword(button) {
+    const pass = document.getElementById("password");
+
+    if (pass.type === "password") {
+        pass.type = "text";
+        button.innerText = "Hide";
+    } else {
+        pass.type = "password";
+        button.innerText = "Show";
+    }
+}
+</script>
+
+</body>
 </html>
-
